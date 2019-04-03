3c12e41ddadfc04bab8d29b649d2656b6cfeabaa
CarlRWarren <cwarren@student.neumont.edu>
2019-04-03 14:38:20 -0600

    algorithm

---
+++ IsomorphicStrings/IsomorphicStrings/IsomorphicStrings/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsomorphicStrings
+{
+    class Program
+    {
+        public Dictionary<string, List<string>> exactIsomorphs = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> looseIsomorphs = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> nonIsomorphs = new Dictionary<string, List<string>>();
+        static void Main(string[] args)
+        {
+            ReadFile();
+        }
+
+        static void ReadFile()
+        {
+            Console.WriteLine("What is the file path?");
+            string filePath = Console.ReadLine();
+            string[] wordsFromFile = System.IO.File.ReadAllLines(@filePath);
+            DetermineExacts(wordsFromFile);
+            DetermineLoose(wordsFromFile);
+            DetermineNons(wordsFromFile);
+        }
+
+        static void DetermineExacts(string[] words)
+        {
+            List<string> exactKeys = null;
+            Dictionary<string, List<string>> keyToWord = null;
+            foreach (string word in words)
+            {
+                string key = DetermineExactKey(word) + ":";
+                if (!exactKeys.Contains(key))
+                {
+                    exactKeys.Add(key);
+                }
+                keyToWord[key].Add(word);
+            }
+        }
+
+        static string DetermineExactKey(string word)
+        {
+            string key = "";
+            int index = 0;
+            List<char> letters = null;
+            foreach(char letter in word)
+            {
+                if (!letters.Contains(letter))
+                {
+                    letters.Add(letter);
+                    key += index;
+                    index++;
+                }
+                else
+                {
+                    key += index;
+                }
+            }
+            return key;
+        }
+
+        static void DetermineLoose(string[] words)
+        {
+            foreach (string word in words)
+            {
+
+            }
+        }
+
+        static void DetermineNons(string[] words)
+        {
+            foreach (string word in words)
+            {
+
+            }
+        }
+    }
+}
---
+++ ThumbDirectory/ThumbDirectory/ThumbDirectory/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ThumbDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Phone("/+1-541-754-3010 156 Alphand_St. <J Steeve>\n", "1-541-754-3010"));
+        }
+        public static string Phone(string strng, string num)
+        {
+            string result = "";
+            string[] lines = strng.Split('\n');
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+                Regex numReg = new Regex($"{num}");
+                Console.WriteLine(numReg);
+
+                MatchCollection matches = numReg.Matches(line);
+                if(matches.Count != 0)
+                {
+                    Regex nameReg = new Regex(@"<[\w\s]+>");
+                    MatchCollection nameMatches = nameReg.Matches(line);
+                    if (nameMatches.Count > 1)
+                    {
+                        result = "Error => Too many people: nb";
+                    }
+                    else
+                    {
+                        string name = "";
+                        foreach(var mc in nameMatches)
+                        {
+                            //mc[]
+                        }
+                        result = $"Phone => {num}, Name => {name}";
+                    }
+
+                }
+                else
+                {
+                    result = "Error => Not Found: nb";
+                }
+            }
+            //MatchCollection matches = nameReg.Matches(testStr);
+            //Console.WriteLine(matches.Count);
+            return result;
+        }
+    }
+}
